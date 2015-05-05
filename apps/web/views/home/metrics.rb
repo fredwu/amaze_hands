c71bfe37dbbd7f0c0c860e5a8473c6c0947906ad
Fred Wu <ifredwu@gmail.com>
2015-05-05 10:27:41 +1000

    Defaults to Pricing and fortnightly stats

--- apps/web/views/home/metrics.rb
+++ apps/web/views/home/metrics.rb
@@ -2,14 +2,14 @@ module Web::Views::Home
   class Metrics
     include Web::View
 
-    WEEK_FREQUENCY = 1
+    WEEK_FREQUENCY = 2
 
     def render
       Web::Presenters::JSONPresenter.new(
         Workflow.new(
           strategy: Strategies::LeanKit,
-          lanes:    Strategies::LeanKit::CustomersLanes,
-          files: Dir["#{Web::Application.configuration.root}/../../db/customers/*.txt"]
+          lanes:    Strategies::LeanKit::PricingLanes,
+          files: Dir["#{Web::Application.configuration.root}/../../db/pricing/*.txt"]
         )
       ).metrics(
         measure_every: WEEK_FREQUENCY.weeks
