e625dcbe1acad18dd638ebbeef660b8eb52b30cf
Fred Wu <ifredwu@gmail.com>
2015-05-21 16:52:38 +1000

    Updated to show metrics for all squads

--- apps/web/views/home/metrics.rb
+++ apps/web/views/home/metrics.rb
@@ -6,14 +6,40 @@ module Web::Views::Home
 
     def render
       raw Web::Presenters::JSONPresenter.new(
-        Workflow.new(
-          strategy: Strategies::LeanKit,
-          lanes:    Strategies::LeanKit::PricingLanes,
-          files:    Dir["#{Web::Application.configuration.root}/../../db/pricing/*.txt"]
-        )
+        Workflow.new(workflow_params)
       ).metrics(
         measure_every: WEEK_FREQUENCY.weeks
       )
+    end
+
+    private
+
+    def workflow_params
+      send :"workflow_params_for_#{params[:name]}"
+    end
+
+    def workflow_params_for_customers
+      {
+        strategy: Strategies::LeanKit,
+        lanes:    Strategies::LeanKit::CustomersLanes,
+        files:    Dir["#{Web::Application.configuration.root}/../../db/customers/*.txt"]
+      }
+    end
+
+    def workflow_params_for_pricing
+      {
+        strategy: Strategies::LeanKit,
+        lanes:    Strategies::LeanKit::PricingLanes,
+        files:    Dir["#{Web::Application.configuration.root}/../../db/pricing/*.txt"]
+      }
+    end
+
+    def workflow_params_for_products
+      {
+        strategy: Strategies::CSV,
+        lanes:    Strategies::CSV::Lanes,
+        files:    Dir["#{Web::Application.configuration.root}/../../db/products/*.csv"]
+      }
     end
   end
 end
